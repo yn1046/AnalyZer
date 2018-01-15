1d0a9a9527afa04e79eeb8b6660722250acda11a
yn1046 <wantedblood@2ch.hk>
2018-01-15 13:39:18 +0300

    Span calculation implemented

---
+++ AnalyZer/Extensions/ExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalyZer.Extensions
+{
+    public static class ExtensionMethods
+    {
+        public static int GetIndentation(this string str)
+        {
+            var count = 0;
+            for (var i = 0; str[i] == ' '; i++) count++;
+            return count;
+        }
+    }
+}
--- AnalyZer/ViewModels/MainViewModel.cs
+++ AnalyZer/ViewModels/MainViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using AnalyZer.Extensions;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -13,7 +15,8 @@ namespace AnalyZer.ViewModels
     {
         public Models.Main Model { get; set; }
 
-        public ICommand DoAnalyze { get; set; }
+        public ICommand DoAnalyzeGilb { get; set; }
+        public ICommand DoAnalyzeChepin { get; set; }
 
         public string[] ops = { "+", "-", "*", "/", "%", "**", "//" , "==", "!=",
         "<>", ">", "<", ">=", "<=", "=", "+=", "-=", "*=", "/=", "%=", "**=", "//=",
@@ -21,18 +24,91 @@ namespace AnalyZer.ViewModels
 
         public MainViewModel()
         {
-            DoAnalyze = new DelegateCommand(Analyze);
+            DoAnalyzeGilb = new DelegateCommand(AnalyzeGilb);
+            DoAnalyzeChepin = new DelegateCommand(AnalyzeChepin);
             Model = new Models.Main();
+            try
+            {
+                Model.Text = File.ReadAllText("progExample.txt");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Error reading file!\n{e.Message}", "Error", MessageBoxButton.OK);
+            }
         }
 
-        public void Analyze()
+        public void AnalyzeGilb()
         {
-            var count = ops.Where(o => Model.Text.Contains(o)).Count();
             var UsedBranchingOperators = Model.Text.Split(' ').Where(el => (new Regex("((^if)|(^else)|(^elif))(:*)(\r*)(\n*)")).IsMatch(el)).ToList();
-            var countBranches = UsedBranchingOperators.Count;
-            MessageBox.Show($"Length: {Model.Text.Split('\n').Count()}\n" +
-                $"Operators: {count}\n" +
-                $"Branches: {countBranches}", "Results", MessageBoxButton.OK);
+            var Loops = Model.Text.Split(' ').Where(el => (new Regex("((^)|( ))(for|while)")).IsMatch(el)).ToList();
+            var GilbCL = UsedBranchingOperators.Count + Loops.Count;
+            var LinesWithOperators = Model.Text.Split('\n').Where(l => ops.Any(o => l.Contains(o))).ToList();
+            var Gilbcl = Math.Round((double)GilbCL / LinesWithOperators.Count, 3);
+
+            var LinesList = Model.Text.Split('\n').ToList();
+            int cliHere = 0, cli = 0, indHere, indExtIf = 0;
+            bool underIf = false;
+            for (var i = 0; i < LinesList.Count - 1; i++)
+            {
+                indHere = LinesList[i].GetIndentation();
+
+                if (underIf && indExtIf >= indHere)
+                {
+                    underIf = false;
+                    cliHere = 0;
+                }
+                if (underIf && (new Regex("( |^)(if |else|elif)")).IsMatch(LinesList[i]))
+                {
+                    cliHere++;
+                    indExtIf = indHere;
+                }
+                if (!underIf && (new Regex("( |^)(if |else|elif)")).IsMatch(LinesList[i]))
+                {
+                    underIf = true;
+                    indExtIf = indHere;
+                    cliHere = 0;
+                }
+
+                if (cliHere > cli) cli = cliHere;
+            }
+
+            MessageBox.Show($"Code length: {Model.Text.Split('\n').Count()}\n" +
+                $"Branches & loops (CL): {GilbCL}\n" +
+                $"cl: {Gilbcl}\n" +
+                $"cli: {cli}", "Results", MessageBoxButton.OK);
         }
+
+        public void AnalyzeChepin()
+        {
+            var Lines = Model.Text.Split('\n').ToList();
+            var idList = new List<string>();
+
+            var result = "Classes and functions span:\n";
+            FilterLines(Lines, @"(def|class) (.*)\(", ref idList, ref result);
+
+            result += "\nVariables span:\n";
+            FilterLines(Lines, @"( |^)+(\w+) = ", ref idList, ref result);
+
+            MessageBox.Show(result, "Results", MessageBoxButton.OK);
+        }
+
+        private void FilterLines(List<string> Lines, string reg, ref List<string> idList, ref string result)
+        {
+            foreach (var line in Lines)
+            {
+                var matchId = Regex.Match(line, reg);
+                if (matchId.Success)
+                {
+                    var id = matchId.Groups[2].Value.Trim();
+                    if (!idList.Contains(id))
+                    {
+                        idList.Add(id);
+                        var count = Lines.Where(l => (new Regex($@"[^\']\b({id})\b[^\']")).IsMatch(l)).Count() - 1;
+                        if (count != 0) result += $"{id}: {count}\n";
+                    }
+                }
+            }
+        }
+
     }
 }
